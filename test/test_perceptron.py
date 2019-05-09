27a3d01f24e66e6a91fe696a3915369c171577f5
szk2s <suzukisjss@gmail.com>
2019-05-10 00:50:43 +0900

    Add test for perceptron
    - nor gate
    - xor gate

--- test/test_perceptron.py
+++ test/test_perceptron.py
@@ -28,3 +28,26 @@ class TestPerceptron(object):
         assert or_gate.feed(np.array([1, 0])) == 1
         assert or_gate.feed(np.array([0, 1])) == 1
         assert or_gate.feed(np.array([1, 1])) == 1
+
+    def test_nor_gate(self):
+        or_gate = Perceptron(np.array([-0.5, -0.5]), 0.25)
+
+        assert or_gate.feed(np.array([0, 0])) == 1
+        assert or_gate.feed(np.array([1, 0])) == 0
+        assert or_gate.feed(np.array([0, 1])) == 0
+        assert or_gate.feed(np.array([1, 1])) == 0
+
+    def test_xor_gate(self):
+        and_gate = Perceptron(np.array([0.5, 0.5]), -0.75)
+        nand_gate = Perceptron(np.array([-0.5, -0.5]), 0.75)
+        or_gate = Perceptron(np.array([0.5, 0.5]), -0.25)
+
+        def xor_gate(x: np.array):
+            s1 = nand_gate.feed(x)
+            s2 = or_gate.feed(x)
+            return and_gate.feed(np.array([s1, s2]))
+
+        assert xor_gate(np.array([0, 0])) == 0
+        assert xor_gate(np.array([1, 0])) == 1
+        assert xor_gate(np.array([0, 1])) == 1
+        assert xor_gate(np.array([1, 1])) == 0
