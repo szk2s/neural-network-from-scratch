3b9fdbabe9d03d13d1ed8051c22c819dea128df7
szk2s <suzukisjss@gmail.com>
2019-05-10 11:58:23 +0900

    Refactor
    - Make perceptron module functional
    - Add make_gate function
    - Cleaner module import statement

--- perceptron.py
+++ perceptron.py
@@ -1,16 +1,36 @@
 import numpy as np
+from typing import Callable
 
 
-class Perceptron:
-    """
-    2 in 1 out
-    """
-
-    def __init__(self, w: np.array, bias: float):
-        self.w = w
-        self.bias = bias
-
-    def feed(self, x: np.array):
-        if np.dot(x, self.w.T) + self.bias > 0:
+def make_perceptron(w: np.array, bias: float) -> Callable:
+    def perceptron(x: np.array):
+        if w.shape != x.shape:
+            raise Exception('The shape of x does not match one of w')
+        if np.dot(x, w.T) + bias > 0:
             return 1
         return 0
+
+    return perceptron
+
+
+def make_gate(gate_type: str) -> Callable:
+    if gate_type == 'and':
+        return make_perceptron(np.array([0.5, 0.5]), -0.75)
+    if gate_type == 'nand':
+        return make_perceptron(np.array([-0.5, -0.5]), 0.75)
+    if gate_type == 'or':
+        return make_perceptron(np.array([0.5, 0.5]), -0.25)
+    if gate_type == 'nor':
+        return make_perceptron(np.array([-0.5, -0.5]), 0.25)
+    if gate_type == 'xor':
+        and_gate = make_gate('and')
+        nand_gate = make_gate('nand')
+        or_gate = make_gate('or')
+
+        def xor_gate(x: np.array):
+            s1 = nand_gate(x)
+            s2 = or_gate(x)
+            return and_gate(np.array([s1, s2]))
+
+        return xor_gate
+    raise Exception('gate_type should be one of the next. "and", "nand", "or", "nor", "xor".')
--- test/test_perceptron.py
+++ test/test_perceptron.py
@@ -1,51 +1,42 @@
-from .context import perceptron
+from .context import *
 import numpy as np
-
-Perceptron = perceptron.Perceptron
 
 
 class TestPerceptron(object):
     def test_and_gate(self):
-        and_gate = Perceptron(np.array([0.5, 0.5]), -0.75)
+        and_gate = make_gate('and')
 
-        assert and_gate.feed(np.array([0, 0])) == 0
-        assert and_gate.feed(np.array([1, 0])) == 0
-        assert and_gate.feed(np.array([0, 1])) == 0
-        assert and_gate.feed(np.array([1, 1])) == 1
+        assert and_gate(np.array([0, 0])) == 0
+        assert and_gate(np.array([1, 0])) == 0
+        assert and_gate(np.array([0, 1])) == 0
+        assert and_gate(np.array([1, 1])) == 1
 
     def test_nand_gate(self):
-        nand_gate = Perceptron(np.array([-0.5, -0.5]), 0.75)
+        nand_gate = make_gate('nand')
 
-        assert nand_gate.feed(np.array([0, 0])) == 1
-        assert nand_gate.feed(np.array([1, 0])) == 1
-        assert nand_gate.feed(np.array([0, 1])) == 1
-        assert nand_gate.feed(np.array([1, 1])) == 0
+        assert nand_gate(np.array([0, 0])) == 1
+        assert nand_gate(np.array([1, 0])) == 1
+        assert nand_gate(np.array([0, 1])) == 1
+        assert nand_gate(np.array([1, 1])) == 0
 
     def test_or_gate(self):
-        or_gate = Perceptron(np.array([0.5, 0.5]), -0.25)
+        or_gate = make_gate('or')
 
-        assert or_gate.feed(np.array([0, 0])) == 0
-        assert or_gate.feed(np.array([1, 0])) == 1
-        assert or_gate.feed(np.array([0, 1])) == 1
-        assert or_gate.feed(np.array([1, 1])) == 1
+        assert or_gate(np.array([0, 0])) == 0
+        assert or_gate(np.array([1, 0])) == 1
+        assert or_gate(np.array([0, 1])) == 1
+        assert or_gate(np.array([1, 1])) == 1
 
     def test_nor_gate(self):
-        or_gate = Perceptron(np.array([-0.5, -0.5]), 0.25)
+        nor_gate = make_gate('nor')
 
-        assert or_gate.feed(np.array([0, 0])) == 1
-        assert or_gate.feed(np.array([1, 0])) == 0
-        assert or_gate.feed(np.array([0, 1])) == 0
-        assert or_gate.feed(np.array([1, 1])) == 0
+        assert nor_gate(np.array([0, 0])) == 1
+        assert nor_gate(np.array([1, 0])) == 0
+        assert nor_gate(np.array([0, 1])) == 0
+        assert nor_gate(np.array([1, 1])) == 0
 
     def test_xor_gate(self):
-        and_gate = Perceptron(np.array([0.5, 0.5]), -0.75)
-        nand_gate = Perceptron(np.array([-0.5, -0.5]), 0.75)
-        or_gate = Perceptron(np.array([0.5, 0.5]), -0.25)
-
-        def xor_gate(x: np.array):
-            s1 = nand_gate.feed(x)
-            s2 = or_gate.feed(x)
-            return and_gate.feed(np.array([s1, s2]))
+        xor_gate = make_gate('xor')
 
         assert xor_gate(np.array([0, 0])) == 0
         assert xor_gate(np.array([1, 0])) == 1
